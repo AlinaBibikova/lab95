15d92829ee730bce5718557738f34b8f20029567
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-05-21 18:14:58 +0600

    #1 added in models Cocktail

---
+++ api/models/Cocktail.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CocktailSchema = new Schema ({
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User',
+    },
+    nameCocktail: {
+        type: String,
+        required: true,
+    },
+    image: String,
+    recipe: {
+        type: String,
+        required: true,
+    },
+    published: {
+        type: Boolean,
+        default: false,
+        enum: [true, false]
+    },
+    ingredients: [
+        {nameIngredient: String,
+        amount: String,
+        position: {type: Number, required: true}}
+        ],
+    rating: [{userId: String,
+         grade: String}]
+});
+
+const Cocktail = mongoose.model('Cocktail', CocktailSchema);
+
+module.exports = Cocktail;
