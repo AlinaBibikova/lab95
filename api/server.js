97ad91709a04a03c304c7c6de20381d856d605da
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-05-21 13:23:53 +0600

    #1 added server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config');
+const cors = require('cors');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+
 const port = 8000;
 
+mongoose.connect(config.dbUrl, config.mongoOptions).then( () => {
+
+});
+
+app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+});
