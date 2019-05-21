064917b7102d356c85c40f1c4b8328e7c0cecf4a
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-05-21 13:50:45 +0600

    #1 added in models User

--- api/config.js
+++ api/config.js
@@ -9,5 +9,9 @@ module.exports = {
     mongoOptions: {
         useNewUrlParser: true,
         useCreateIndex: true,
+    },
+    facebook: {
+        appId: '545138079223618',
+        appSecret: '739a3bb638b68d5d77acd655a8248dbd' // insecure!
     }
 };
