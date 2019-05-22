f0ae46a160893f0dba831b96eb82b4db423b2a05
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-05-23 00:17:09 +0600

    #1 added user sessions

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,6 +6,19 @@ const User = require('../models/User');
 const router = express.Router();
 const config = require('../config');
 
+router.post('/sessions', async (req, res) => {
+    const user = await User.findOne({username: req.body.username});
+    if (!user) return res.status(400).send({message: 'Username or Password incorrect!'});
+
+    const isMatch = await user.checkPassword(req.body.password);
+    if (!isMatch) return res.status(400).send({message: 'Username or Password incorrect!'});
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({message: 'Login successful!', user});
+});
+
 router.post('/facebookLogin', async (req, res) => {
     const inputToken = req.body.accessToken;
     const accessToken = config.facebook.appId + '|' + config.facebook.appSecret;
