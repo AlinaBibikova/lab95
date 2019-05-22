41d3c00ab091e438e4b7a8514d5d5344e0c70484
Alina Bibikova <alinochkakleopatra@gmail.com>
2019-05-22 19:33:15 +0600

    #1 added in routes cocktails, middleware

--- api/server.js
+++ api/server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const users = require('./routes/users');
+const cocktails = require('./routes/cocktails');
 
 app.use(cors());
 
@@ -16,6 +17,7 @@ const port = 8000;
 
 mongoose.connect(config.dbUrl, config.mongoOptions).then( () => {
     app.use('/users', users);
+    app.use('/cocktails', cocktails);
 });
 
 app.listen(port, () => {
